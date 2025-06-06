01920d6842a84fe73391fe227561b0bd5c0278d7
Brajesh <brajeshsullere305@gmail.com>
2025-06-06 22:10:33 +0530

    first commit

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,3 +1,5 @@
+import { config } from "dotenv";
+config({ path: "./config/config.env" });
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
   const options = {
@@ -5,6 +7,8 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true, // Set httpOnly to true
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    secure: process.env.NODE_ENV === "production",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
